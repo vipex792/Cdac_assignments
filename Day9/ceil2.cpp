a1a9261e0096434662895d5facc293b5c563ed5b
Tanntech <1320203939@bitraipur.ac.in>
2025-12-06 16:44:13 +0530

    Uploading C++ assignments and Labwork

--- Day9/ceil2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>  
-#include<cmath>  
-using namespace std;  
-int main()  
-{  
-  float x=-2.2;  
-  cout<<"final value of x is :"<<ceil(x);  
-  return 0;  
-}  
--- Day9/standardNamespace.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-int main()
-{
-	std::cout<<"enter no to add";
-	int num1,num2;
-	std::cin>>num1;
-	std::cin>>num2;
-	std::cout<<"addition is"<<num1+num2;
-}
